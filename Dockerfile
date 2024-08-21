927e59fba402a94fb2aacbefba656f25cb14217d
luizgustavojunqueira <luizgustavossj@gmail.com>
2024-08-20 20:04:25 -0400

    ci: add docker file for the api

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM golang:1.23.0
+
+WORKDIR /usr/src/app
+
+COPY go.mod go.sum ./
+
+RUN go mod download
+
+COPY . .
+
+RUN go build -o ./bin/main ./cmd/main.go
+
+CMD ["./bin/main"]
--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"luizg/PostsAPI/api"
+	"os"
 )
 
 func main() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
 	api := &api.Api{}
-	api.Run(":8080")
+	port := os.Getenv("API_PORT")
+	api.Run(":" + port)
 }
